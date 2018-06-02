eaac0ee2cbd56d69ac52b7eff2bb5d02d8245425
mahou <tyanmahou@gmail.com>
2018-06-02 17:30:07 +0900

    add sample code

--- CppUFCS/main.cpp
+++ CppUFCS/main.cpp
@@ -1,7 +1,43 @@
 #include<iostream>
+#include<cstdio>
 #include<CppUFCS.hpp>
+
+struct Hoge
+{
+	int func(int a)
+	{
+		return a + 2;
+	}
+	void printf() {
+		std::cout << "Hoge" << std::endl;
+	}
+};
+
+struct Foo
+{};
+
+int func(Foo foo, int a)
+{
+	return a * 2;
+}
+
+namespace UFCS
+{
+	USE_UFCS(func);
+
+	//If a free function is defined in the namespace 
+	USE_UFCS(printf,std::);
+}
 
 int main()
 {
+	using namespace UFCS;
+	Hoge hoge;
+	Foo foo;
+
+	int result = (hoge >> func_(1)) + (foo >> func_(2));
+
+	"result is %d\n" >> printf_(result);
+
 	return 0;
 }
